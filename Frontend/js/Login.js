9cfb50d5d58e0136ec5d38e5219720c1f1981cce
cesarqa25 <c.quirozayala@uandresbello.edu>
2025-09-24 19:20:54 -0300

    sprint 1

--- Frontend/js/Login.js
+++ Frontend/js/Login.js
@@ -1,3 +1,4 @@
+/*
 const form = document.querySelector('form');
 const $username = document.getElementById('username');
 const $password = document.getElementById('password');
@@ -41,4 +42,25 @@ form.addEventListener('submit', async (e) => {
   } finally {
     $submit.disabled = false;
   }
-});
+});*/
+
+const $submit = document.getElementById("submit"),
+  $password = document.getElementById("password"),
+  $username = document.getElementById("username"),
+  $visible = document.getElementById("visible");
+
+document.addEventListener("change", (e)=>{
+  if(e.target === $visible){
+    if($visible.checked === false) $password.type = "password";
+    else $password.type = "text";
+  }
+})
+
+document.addEventListener("click", (e)=>{
+  if(e.target === $submit){
+    if($password.value !== "" && $username.value !== ""){
+      e.preventDefault();
+      window.location.href = "dashboard.html";
+    }
+  }
+})
